7ebbb79f0bd9ac7b00056fe4835d4ed57ae75d9b
Yadh <yadhnikawakde@gmail.com>
2025-06-02 14:36:34 +0530

    F for respect

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse, RedirectResponse
+
+app = FastAPI()
+
+@app.get("/F")
+async def send_respect():
+    return {"message": "Respect sent!"}
+
+@app.get("/")
+async def send_respect():
+    return {"command": "Click PrettyPrint",
+            "about me" :"I am Yadh \nI code python \nI lay in the Backends",
+            "some info" : "I don't fw html/js",
+            "message" : "Press F to pay respect (obv in the url)"}
+ 
+@app.get("/{anything_else}")
+async def bro_press_f(anything_else: str):
+    return JSONResponse(
+        status_code=400,
+        content={"message": "bro press F"}
+    )
